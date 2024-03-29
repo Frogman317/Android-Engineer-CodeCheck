4c71fa36d9eb54a08fa4022d0cff8a39f8090186
Y_Nishino <kaek33586@gmail.com>
2024-03-30 00:56:44 +0900

    API部分のコードを分割

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/api/GithubAPI.kt
@@ -0,0 +1,56 @@
+package jp.co.yumemi.android.code_check.api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.statement.HttpResponse
+import jp.co.yumemi.android.code_check.model.Repository
+import org.json.JSONArray
+import org.json.JSONObject
+
+class GithubAPI {
+    companion object {
+        suspend fun getData(keyword : String): List<Repository> {
+            val client = HttpClient(Android)
+            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+                header("Accept", "application/vnd.github.v3+json")
+                parameter("q", keyword)
+            }
+
+            val jsonBody = JSONObject(response.receive<String>())
+            val jsonItems = jsonBody.optJSONArray("items") ?: JSONArray()
+            val repositories = mutableListOf<Repository>()
+
+            /**
+             * アイテムの個数分ループする
+             */
+            for (i in 0 until jsonItems.length()) {
+                val jsonItem = jsonItems.optJSONObject(i)!!
+                val name = jsonItem.optString("full_name")
+                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
+                val language = jsonItem.optString("language")
+                val stargazersCount = jsonItem.optLong("stargazers_count")
+                val watchersCount = jsonItem.optLong("watchers_count")
+                val forksCount = jsonItem.optLong("forks_conut")
+                val openIssuesCount = jsonItem.optLong("open_issues_count")
+
+                repositories.add(
+                    Repository(
+                        name = name,
+                        ownerIconUrl = ownerIconUrl,
+                        language = language,
+                        stargazersCount = stargazersCount,
+                        watchersCount = watchersCount,
+                        forksCount = forksCount,
+                        openIssuesCount = openIssuesCount
+                    )
+                )
+            }
+
+            return repositories.toList()
+        }
+    }
+}
