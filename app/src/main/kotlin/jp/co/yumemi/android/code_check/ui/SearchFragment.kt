bc9884259d4a9d4f501bdb4ef8641c2ff1d61428
Y_Nishino <kaek33586@gmail.com>
2024-03-30 00:59:49 +0900

    APIを分割したことでのImportなどの調整

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/SearchFragment.kt
@@ -7,12 +7,14 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentSearchBinding
 import jp.co.yumemi.android.code_check.model.Repository
+import kotlinx.coroutines.launch
 
 class SearchFragment: Fragment(R.layout.fragment_search)
 {
@@ -20,36 +22,36 @@ class SearchFragment: Fragment(R.layout.fragment_search)
     {
         super.onViewCreated(view, savedInstanceState)
 
-        val _binding= FragmentSearchBinding.bind(view)
+        val binding= FragmentSearchBinding.bind(view)
 
-        val _viewModel= SearchViewModel(requireContext())
+        val viewModel= SearchViewModel()
 
-        val _layoutManager= LinearLayoutManager(requireContext())
-        val _dividerItemDecoration=
-            DividerItemDecoration(requireContext(), _layoutManager.orientation)
-        val _adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener {
+        val layoutManager= LinearLayoutManager(requireContext())
+        val dividerItemDecoration=
+            DividerItemDecoration(requireContext(), layoutManager.orientation)
+        val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener {
             override fun itemClick(repository: Repository){
                 gotoRepositoryFragment(repository)
             }
         })
 
-        _binding.searchInputText
+        binding.searchInputText
             .setOnEditorActionListener{ editText, action, _ ->
                 if (action== EditorInfo.IME_ACTION_SEARCH){
-                    editText.text.toString().let {
-                        _viewModel.searchResults(it).apply{
-                            _adapter.submitList(this)
-                        }
+                    val inputText = editText.text.toString()
+                    lifecycleScope.launch {
+                        val searchResult = viewModel.searchResults(inputText)
+                        adapter.submitList(searchResult)
                     }
                     return@setOnEditorActionListener true
                 }
                 return@setOnEditorActionListener false
             }
 
-        _binding.recyclerView.also{
-            it.layoutManager= _layoutManager
-            it.addItemDecoration(_dividerItemDecoration)
-            it.adapter= _adapter
+        binding.recyclerView.also{
+            it.layoutManager= layoutManager
+            it.addItemDecoration(dividerItemDecoration)
+            it.adapter= adapter
         }
     }
 
