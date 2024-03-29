94361c08cc3fefb69e62ed064aa6db0f7d4e58b9
Y_Nishino <kaek33586@gmail.com>
2024-03-30 00:58:59 +0900

    日時の取得は必要ないので削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/DetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/DetailFragment.kt
@@ -4,13 +4,11 @@
 package jp.co.yumemi.android.code_check.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
 import jp.co.yumemi.android.code_check.R
-import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.databinding.FragmentDetailBinding
 import jp.co.yumemi.android.code_check.model.Repository
 
@@ -23,8 +21,6 @@ class DetailFragment : Fragment(R.layout.fragment_detail)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        Log.d("検索した日時", lastSearchDate.toString())
 
         binding = FragmentDetailBinding.bind(view)
 
