79fe4b341ab386eae5accd00e4c9ff3d591d3438
Y_Nishino <kaek33586@gmail.com>
2024-03-30 15:14:46 +0900

    テキストの文字列結合をstrings.xmlに変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/fragment/DetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/fragment/DetailFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check.ui
+package jp.co.yumemi.android.code_check.ui.fragment
 
 import android.os.Bundle
 import android.view.View
@@ -11,6 +11,7 @@ import coil.load
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentDetailBinding
 import jp.co.yumemi.android.code_check.model.Repository
+import jp.co.yumemi.android.code_check.ui.DetailFragmentArgs
 
 class DetailFragment : Fragment(R.layout.fragment_detail)
 {
@@ -26,12 +27,12 @@ class DetailFragment : Fragment(R.layout.fragment_detail)
 
         val repository: Repository = args.repository
 
-        _binding.ownerIconView.load(repository.ownerIconUrl);
-        _binding.nameView.text = repository.name;
-        _binding.languageView.text = repository.language;
-        _binding.starsView.text = "${repository.stargazersCount} stars";
-        _binding.watchersView.text = "${repository.watchersCount} watchers";
-        _binding.forksView.text = "${repository.forksCount} forks";
-        _binding.openIssuesView.text = "${repository.openIssuesCount} open issues";
+        _binding.ownerIconView.load(repository.ownerIconUrl)
+        _binding.nameView.text = repository.name
+        _binding.languageView.text = repository.language
+        _binding.starsView.text = getString(R.string.stargazersCount, repository.stargazersCount.toString())
+        _binding.watchersView.text = getString(R.string.watchersCount, repository.watchersCount.toString())
+        _binding.forksView.text = getString(R.string.forksCount, repository.forksCount.toString())
+        _binding.openIssuesView.text = getString(R.string.openIssuesCount, repository.openIssuesCount.toString())
     }
 }
